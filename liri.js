ebe1a6e80bb4213717f64684089a843b3933679c
Mario Vizcaino <vizhar1952@gmail.com>
2019-11-20 19:18:22 -0500

    working on liri.js. Made changes in other files

--- liri.js
+++ liri.js
@@ -1,31 +1,63 @@
+// Read and set environment variables
 require("dotenv").config();
 
-const keys = require("./keys.js");
-let fs = require("fs");
-let Spotify = require("node-spotify-api");
-let spotify = new Spotify(keys.spotify);
-let request = require("request");
-let userChoice = process.argv[2];
-let choiceParameter = process.argv[3];
+//VARS
+var request = require("request");
+var fs = require("fs");
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+//vars to capture user inputs.
+var userOption = process.argv[2]; 
+var inputParameter = process.argv[3];
 
+//Execute function
+UserInputs(userOption, inputParameter);
 
-function userInput (userChoice, choiceParameter)
-{
-switch (userChoice)
-{
-    case "spotify-this-song":
-        showSongInfo(choiceParameter);
+//FUNCTIONS
+function UserInputs (userOption, inputParameter){
+    switch (userOption) {
+    case 'concert-this':
+        showConcertInfo(inputParameter);
         break;
-    case "concert-this":
-        showConcertInfo(choiceParameter);
+    case 'spotify-this-song':
+        showSongInfo(inputParameter);
         break;
-    case "movie-this":
-        showMovieInfo(choiceParameter);
+    case 'movie-this':
+        showMovieInfo(inputParameter);
         break;
-    case "do-what-it-says":
-        showSomeInfo(choiceParameter);
+    case 'do-what-it-says':
+        showSomeInfo();
         break;
-    default:
+    default: 
         console.log("Invalid Option. Please type any of the following options: \nconcert-this \nspotify-this-song \nmovie-this \ndo-what-it-says")
+    }
 }
+
+//Funtion for Concert Info: Bands in Town
+function showConcertInfo(inputParameter){
+    var queryUrl = "https://rest.bandsintown.com/artists/" + inputParameter + "/events?app_id=codingbootcamp";
+    request(queryUrl, function(error, response, body) {
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+        var concerts = JSON.parse(body);
+        for (var i = 0; i < concerts.length; i++) {  
+            console.log("**********EVENT INFO*********");  
+            fs.appendFileSync("log.txt", "**********EVENT INFO*********\n");//Append in log.txt file
+            console.log(i);
+            fs.appendFileSync("log.txt", i+"\n");
+            console.log("Name of the Venue: " + concerts[i].venue.name);
+            fs.appendFileSync("log.txt", "Name of the Venue: " + concerts[i].venue.name+"\n");
+            console.log("Venue Location: " +  concerts[i].venue.city);
+            fs.appendFileSync("log.txt", "Venue Location: " +  concerts[i].venue.city+"\n");
+            console.log("Date of the Event: " +  concerts[i].datetime);
+            fs.appendFileSync("log.txt", "Date of the Event: " +  concerts[i].datetime+"\n");
+            console.log("*****************************");
+            fs.appendFileSync("log.txt", "*****************************"+"\n");
+        }
+    } else{
+      console.log('Error occurred.');
+    }
+});
 }
+
